c8aa214633e31bb0a30b0fa142c75b96043c0d01
xycxz <mateoturina@Mateos-MacBook-Air.local>
2026-04-14 18:41:24 -0300

    Implementing NavBar Component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+  },
+  reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
